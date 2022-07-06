861cda9a5824540ab7b3b7e4b88a594e3e981381
Brian Mugweru <brianmugweru@gmail.com>
2022-07-06 22:17:39 +0300

    ch(pages/404.tsx): add alt for images

--- pages/404.tsx
+++ pages/404.tsx
@@ -13,10 +13,10 @@ const Error: NextPage<ErrorProps> = ({}) => {
   return (
     <div className="bg-[#f5f5f5] dark:bg-[#20201E] relative block">
       <div className="absolute sm:top-28 md:top-32 sm:left-[15%] lg:top-36 xl:top-52 rotate-[340deg] sm:block hidden">
-        <img src="images/svg/colored-x.svg" className="sm:w-16 sm:h-16 md:w-20 md:h20 lg:w-28 lg:h-28"/>
+        <img src="images/svg/colored-x.svg" className="sm:w-16 sm:h-16 md:w-20 md:h20 lg:w-28 lg:h-28" alt="droidcon colored-x icon" />
       </div>
       <div className="absolute sm:top-56 sm:right-[7%] md:top-64 md:right-[7%] lg:top-80 xl:top-96 lg:right-[7%] xl:right-[10%] 2xl:right-[15%] sm:block hidden">
-        <img src="images/element_left.png" className="sm:w-24 sm:h-20 md:w-28 md:h-24 lg:w-36 lg:h-32"/>
+        <img src="images/element_left.png" className="sm:w-24 sm:h-20 md:w-28 md:h-24 lg:w-36 lg:h-32" alt="droidcon element-x icon" />
       </div>
       <section className="error-code-section">
         <div className="w-screen mx-auto text-center pt-32 pb-16 md:pt-28 sm:pt-20 sm:pb-16 md:pb-20 xl:pt-40 xl:pb-28 lg:pt-28 lg:pb-24">
@@ -38,9 +38,9 @@ const Error: NextPage<ErrorProps> = ({}) => {
           <div className="block">
             <a href="/" className="block items-baseline text-center mx-auto xl:mt-20 lg:mt-16 mt-12">
               {isDarkTheme ? (
-                <img src="images/icons/home-icon-white.svg" className="mr-2 inline" width="18px" />
+                <img src="images/icons/home-icon-white.svg" className="mr-2 inline" width="18px" alt="droidcon home icon white"/>
               ) : (
-                <img src="images/icons/home-icon.svg" className="mr-2 inline" width="18px" />
+                <img src="images/icons/home-icon.svg" className="mr-2 inline" width="18px" alt="droidcon home icon"/>
               )}
               <span className="text-[#000ceb] lg:text-lg align-text-top text-base dark:text-white">Head back home</span>
             </a>
