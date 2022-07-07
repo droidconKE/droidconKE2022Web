b0da9b8b75772c58f431c7484fc6cd555d78df01
Brian Mugweru <brianmugweru@gmail.com>
2022-07-08 01:25:18 +0300

    add error tsx layout

---
+++ components/layouts/error.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import React, { ReactNode } from 'react'
+import { ThemeProvider } from '../../context/ThemeContext'
+import { Footer } from './components/Footer'
+import { NavBar } from './components/NavBar'
+import { NotificationsPrompt } from './components/NotificationsPrompt'
+import { UpdatesAvailablePrompt } from './components/UpdatesAvailablePrompt'
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <ThemeProvider>
+        <div className="w-full bg-white dark:bg-dark">
+          <UpdatesAvailablePrompt />
+          <NavBar />
+          <div>
+            <main>{children}</main>
+          </div>
+          <NotificationsPrompt />
+        </div>
+      </ThemeProvider>
+    </>
+  )
+}
