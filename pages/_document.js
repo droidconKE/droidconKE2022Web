583d7e3627ba31b7ced6c519b20bb95c63d9249c
Magak Emmanuel <emashmagak@gmail.com>
2022-07-08 16:30:52 +0300

    fix viewport

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta charset="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
           <link
             rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
