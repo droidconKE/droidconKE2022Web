12bc84716388e10acb5c2fe5870d9f75714a0336
Magak Emmanuel <emashmagak@gmail.com>
2022-07-08 16:22:43 +0300

    added pr reviews for about

--- components/about/Team.tsx
+++ components/about/Team.tsx
@@ -2,30 +2,32 @@ import { Organizer } from '../../types/types'
 
 function Team({ organizers }: { organizers: Organizer[] }) {
   return (
-    <section className="l-container pb-6 md:pb-12">
+    <section className="l-container">
       <div>
         <div className="items-center flex flex-wrap">
-          <div className="w-full px-0 md:px-24 mr-auto mb-4 md:mb-0 pb-10 lg:pb-16">
-            <div className="w-full flex flex-wrap grid sm:grid-cols-4 md:grid-cols-5 grid-cols-3 gap-2 lg:gap-8 mb-14 md:mb-0">
+          <div className="w-full px-0 md:px-24 mr-auto mb-4 md:mb-0 pb-0 lg:pb-16">
+            <div className="w-full flex-wrap grid sm:grid-cols-4 md:grid-cols-5 grid-cols-3 gap-2 lg:gap-4">
               {organizers.map((org) => (
                 <a
                   key={org.created_at}
                   target="_blank"
                   href={org.link}
-                  className="text-center flex justify-center flex-wrap"
+                  className="text-center"
                   rel="noreferrer"
                 >
-                  <div className="w-24 h-24 md:w-32 md:h-32 p-2 md:p-4 flex justify-center bg-green-c-2 rounded">
+                  <div className="w-24 h-24 md:w-44 md:h-44 p-2 md:p-4 bg-green-c-2 rounded">
                     <img
-                      className="w-full p-0 flex rounded border-2 border-green-500"
+                      className="w-full p-0 flex rounded-lg border border-accent-2"
                       src={org.photo === null ? '/images/icon.png' : org.photo}
                       alt={org.name}
                     />
                   </div>
-                  <p className="text-px-13 font-bold black mt-2 w-full">
-                    {org.name}
-                  </p>
-                  <p className="text-px-12 gray">{org.tagline}</p>
+                  <div className="self-start">
+                    <p className="mt-2 w-full">{org.name}</p>
+                    <p className="text-xs text-light dark:text-light">
+                      {org.tagline}
+                    </p>
+                  </div>
                 </a>
               ))}
             </div>
--- pages/about.tsx
+++ pages/about.tsx
@@ -4,36 +4,44 @@ import type { NextPage } from 'next'
 import { Organizer } from '../types/types'
 import OrganizingTeam from '../components/about/Team'
 import axios from '../utils/axios'
+import Organizers from '../components/home/Organizers'
 
 interface AboutProps {
   organizers: Organizer[]
 }
 
 const About: NextPage<AboutProps> = ({ organizers }) => {
+  const team = organizers.filter((o) => o.type === 'individual')
+  const company = organizers.filter((o) => o.type === 'company')
   return (
     <div
-      className="bg-[length:46%] md:bg-[length:47%] bg-[top_340px_left_110%] md:bg-[top_100px_left_110%] bg-no-repeat"
-      style={{ backgroundImage: 'url(/images/kenyatta.png)' }}
+      className="min-h-screen bg-[length:46%] md:bg-[length:87%] bg-[top_340px_left_115%] md:bg-[top_100px_left_148%] bg-no-repeat"
+      style={{ backgroundImage: 'url(/images/svg/about-bg.svg)' }}
     >
-      <section className="l-container mt-8 md:mt-8 pb-6 md:pb-12">
-        <div className="w-full flex flex-wrap px-4 h-auto lg:h-auto pt-16 md:px-0 py-8 lg:py-20">
+      <section className="s-container mt-8 md:mt-8 pb-24">
+        <div className="w-full flex flex-wrap px-4 min-h-screen pt-16 md:px-0 py-8 lg:py-20">
           <div className="w-full lg:w-5/12 flex items-center">
             <div className="w-full bg-no-repeat bg-center">
               <div className="w-full flex flex-wrap text-left">
-                <h2 className="font-black black  text-4xl md:text-1xl">
+                <h2 className="text-3xl md:text-5xl font-black dark:text-white">
                   ABOUT <br />
                   DROIDCONKE
                 </h2>
-                <p className="text-light dark:text-lighter-dark py-5 md:py-8">
+                <img
+                  className="w-[360px] mt-8 md:mt-10"
+                  src="/images/lines.png"
+                  alt="line"
+                />
+                <p className="text-light dark:text-lighter-dark pt-2 text-sm pr-0 md:pr-10">
                   Droidcon is a global conference focused on the engineering of
                   Android applications. Droidcon provides a forum for developers
                   to network with other developers, share techniques, announce
                   apps and products, and to learn and teach.
-                  <br />
+                  <br /> <br />
                   This two-day developer focused gathering will be held in
                   Nairobi Kenya on August 6th to 8th 2020 and will be the
                   largest of its kind in Africa.
-                  <br />
+                  <br /> <br />
                   It will have workshops and codelabs focused on the building of
                   Android applications and will give participants an excellent
                   chance to learn about the local Android development ecosystem,
@@ -52,27 +60,27 @@ const About: NextPage<AboutProps> = ({ organizers }) => {
           </div>
         </div>
       </section>
-      <section className="w-full l-container md:px-32 sm:px-10">
-        <div className=" mx-auto container px-4 md:py-12">
+      <section className="w-full bg-lighter dark:bg-black-dark">
+        <div className="l-container md:py-12">
           <div className="flex flex-wrap mb-10">
-            <div className="w-full flex flex-wrap justify-center mt-4 md:mt-0 py-2 pb-12">
-              <h4 className="title text-primary font-bold pt-6 md:pt-0">
+            <div className="w-full -mt-28 md:-mt-36 py-2 pb-12">
+              <h4 className="title text-3xl text-primary md:text-6xl dark:text-accent pt-6 md:pt-0">
                 Largest Android Focused <br />
                 Developer Conference <br />
                 in Africa.
               </h4>
             </div>
-            <div className="w-full md:w-6/12 ml-auto px-2 md:px-8">
-              <h3 className="title black underline-green mt-4 md:mt-0">
+            <div className="w-full md:w-6/12 ml-auto pr-0 md:pr-20">
+              <h3 className="title font-bold border-b-8 border-b-accent">
                 DROIDCON
               </h3>
-              <p className="mt-2 md:mt-4 mb-4 md:mb-0 p gray">
+              <p className="mt-2 md:mt-4 mb-4 md:mb-0">
                 Droidcon is the largest global network of developer conferences
                 which brings together the industry's foremost experts dedicated
                 to advancing the Android platform. Droidcon engages a global
                 network of over 25,000+ developers attending our events in 22
                 cities.
-                <br />
+                <br /> <br />
                 Our first Droidcon conference was held in 2009 in Berlin and
                 since then it has spread its influence across the globe and has
                 established itself as the world's foremost community-driven
@@ -83,17 +91,17 @@ const About: NextPage<AboutProps> = ({ organizers }) => {
               </p>
             </div>
 
-            <div className="w-full md:w-6/12 ml-auto px-2 md:px-8">
-              <h3 className="title black underline-green mt-4 md:mt-0">
+            <div className="w-full md:w-6/12 ml-auto pr-0 md:pr-20">
+              <h3 className="title font-bold border-b-8 border-b-accent-2 mt-4 md:mt-0">
                 PAST DROIDCONKE
               </h3>
-              <p className="mt-2 md:mt-4 mb-4 md:mb-0 p gray">
+              <p className="mt-2 md:mt-4 mb-4 md:mb-0">
                 Droidcon is the largest global network of developer conferences
                 which brings together the industry's foremost experts dedicated
                 to advancing the Android platform. Droidcon engages a global
                 network of over 25,000+ developers attending our events in 22
                 cities.
-                <br />
+                <br /> <br />
                 Our first Droidcon conference was held in 2009 in Berlin and
                 since then it has spread its influence across the globe and has
                 established itself as the world's foremost community-driven
@@ -103,26 +111,34 @@ const About: NextPage<AboutProps> = ({ organizers }) => {
                 technologies.
               </p>
             </div>
+            <div className="w-full mt-2 -mb-32 md:-mb-32 pb-20 md:pb-0 flex justify-center pr-0 md:pr-20">
+              <img
+                className="w-20 md:w-36"
+                src="/images/element_left.png"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </section>
-      <section className="w-full container mx-auto h-auto md:px-32 px-4">
+      <section className="l-container mt-4 md:mt-10">
         <div className="flex flex-wrap md:py-16">
           <div className="w-full text-center mb-2 md:mb-0 mt-4 md:mt-0 items-center justify-center">
-            <h3 className="title text-primary font-bold pt-6 md:pt-0">
+            <h3 className="title text-primary dark:text-accent pt-6 md:pt-0">
               Organizing team
             </h3>
           </div>
         </div>
       </section>
-      <OrganizingTeam organizers={organizers} />
+      <OrganizingTeam organizers={team} />
+      <Organizers organizers={company} />
     </div>
   )
 }
 
 export async function getServerSideProps() {
   const organizers = await axios
-    .get(`/organizers/${process.env.ORG_SLUG}/team?type=individual`)
+    .get(`/organizers/${process.env.ORG_SLUG}/team`)
     .then((response) => {
       return response.data.data
     })
