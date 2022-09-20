7f46db45ea5b32c6b3e93dd4346798f3e382b5c0
Brian Mugweru <brianmugweru@gmail.com>
2022-09-20 13:46:29 +0300

    add groupBy3 helper

--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -1,2 +1,3 @@
 export const isServer = typeof window === 'undefined'
 export const isClient = typeof window !== 'undefined'
+export const groupBy3 = arr => arr.reduce((acc, current, index) => (index % 3 ? acc[acc.length - 1].push(current) : acc.push([current])) && acc, [])
