5088131fe965a8f623832b5adc2634234a459a12
Emmanuel Magak <emashmagak@gmail.com>
2022-10-17 12:15:42 +0300

    fixed API keu bug on firebase

--- firebase/util/index.ts
+++ firebase/util/index.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 
 const config = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_NEXT_PUBLIC_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: 'droidconke-70d60.firebaseapp.com',
   projectId: 'droidconke-70d60',
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
