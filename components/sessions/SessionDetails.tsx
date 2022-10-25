47fb359bdfbf950b6a0ee16fc50b9bfbfd86b698
Njeririziki <rizikinjeri@gmail.com>
2022-10-25 21:37:39 +0300

    #62 single session data

--- components/sessions/SessionDetails.tsx
+++ components/sessions/SessionDetails.tsx
@@ -1,7 +1,9 @@
+import { Session } from '../../types/types'
+import { hour } from '../../utils/helpers'
 import { SaveSession } from './SaveSession'
 // import { SessionDetailSkeleton } from './skeletons/SessionDetailSkeleton'
 
-export const SessionDetails = () => {
+export const SessionDetails = ({ session }: { session: Session }) => {
   return (
     <div className="w-full flex-wrap content-start items-start lg:w-5/12 px-0 lg:px-6 flex border-r-0 lg:border-r border-l-0 lg:border-l border-green-200">
       <div className="w-full flex py-4 items-center">
@@ -11,32 +13,37 @@ export const SessionDetails = () => {
         <p className="text-px-14 gray w-full">
           <span className="mr-2 ml-4 lg:ml-0">Level:</span>{' '}
           <span className="uppercase text-xs text-white dark:text-dark text-px-10 bg-black dark:bg-white-dark py-0.5 px-2 rounded-full">
-            #beginner
+            #{session.session_level}
           </span>
         </p>
       </div>
       <div className="w-full flex items-start flex-col">
         <div className="pt-4 md:pt-6">
-          <img
-            className="rounded-lg h-auto md:h-56"
-            src="http://droidcon-erp.herokuapp.com/upload/sessions/4IUDxED2zy.%20png"
-            alt="session"
-          />
+          {session.session_image && (
+            <img
+              className="rounded-lg h-auto md:h-56"
+              src={session.session_image}
+              alt="session"
+            />
+          )}
         </div>
         <h4 className="text-light text-sm dark:text-lighter-dark mt-4">
-          {' '}
-          10:30AM - 11:00AM | Room 1
+          {session.session_format}
         </h4>
-        <p className=" text-xl md:text-2xl mt-1 font-bold">
-          Compose Beyond Material Design
-        </p>
+        <h4 className="text-light text-sm dark:text-lighter-dark mt-2">
+          {' '}
+          {hour(session.start_date_time)} - {hour(session.end_date_time)} |
+          {session.rooms.map((room, i) => (
+            <span key={room.id}>
+              {room.title} {i + 1 < session.rooms.length ? ',' : ''}
+            </span>
+          ))}
+        </h4>
+        <p className=" text-xl md:text-2xl mt-1 font-bold">{session.title}</p>
         <h6 className="text-primary dark:text-white-dark font-bold mt-4 md:mt-10 w-full">
           Session Description:
         </h6>
-        <p className="p gray mt-2">
-          Been in the tech industry for over 20 years. Am passionate about
-          developer communities, motivating people and building successful
-        </p>
+        <p className="p gray mt-2">{session.description}</p>
 
         <div className="w-full justify-center md:justify-start flex flex-col mt-4 md:mt-10 mb-4 lg:mb-16">
           <SaveSession />
--- pages/sessions/[slug].tsx
+++ pages/sessions/[slug].tsx
@@ -3,8 +3,10 @@ import Link from 'next/link'
 import { SessionDetails } from '../../components/sessions/SessionDetails'
 import { ShareSessionAndFeedback } from '../../components/sessions/ShareSessionAndFeedback'
 import { SpeakersDetails } from '../../components/sessions/SpeakersDetails'
+import { Session } from '../../types/types'
+import axios from '../../utils/axios'
 
-const Session: NextPage = () => {
+const Session: NextPage = ({ session }: { session: Session }) => {
   return (
     <div className="w-full mt-16 mb-0">
       <section className="w-full bg-dark dark:bg-black-dark">
@@ -28,12 +30,22 @@ const Session: NextPage = () => {
       <section className="s-container py-2 md:py-4">
         <div className="w-full flex flex-wrap items-start py-0 md:py-3">
           <SpeakersDetails />
-          <SessionDetails />
+          <SessionDetails session={session} />
           <ShareSessionAndFeedback />
         </div>
       </section>
     </div>
   )
 }
+export async function getServerSideProps(context: { query: { slug: string } }) {
+  const { slug } = context.query
+  const session = await axios
+    .get(`/events/${process.env.NEXT_PUBLIC_EVENT_SLUG}/schedule/${slug}`)
+    .then((response) => {
+      return response.data.data
+    })
 
+  // Pass data to the page via props
+  return { props: { session } }
+}
 export default Session
