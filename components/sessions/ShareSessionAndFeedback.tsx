e7b2d071a621b7903f9189dfb2d18b7912f87186
Njeri Kariuki <51441540+njeririziki@users.noreply.github.com>
2022-10-25 23:52:07 +0300

    #62 single session data (#68)
    * #62 single session data
    
    * #64 sessions
    
    Co-authored-by: Emmanuel Magak <emashmagak@gmail.com>

--- components/sessions/ShareSessionAndFeedback.tsx
+++ components/sessions/ShareSessionAndFeedback.tsx
@@ -1,14 +1,57 @@
 import { useState } from 'react'
+import {
+  LinkedinShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  TwitterIcon,
+  LinkedinIcon,
+  TelegramIcon,
+  WhatsappIcon,
+} from 'react-share'
 import { SessionFeedback } from './SessionFeedback'
+import { Session } from '../../types/types'
+import { truncateString } from '../../utils/helpers'
 
-export const ShareSessionAndFeedback = () => {
+export const ShareSessionAndFeedback = ({ session }: { session: Session }) => {
   const [showFeedbackModal, setShowFeedbackModal] = useState(false)
+  const [showShare, setShowShare] = useState(false)
 
   return (
     <div className="w-full px-0 md:px-6 flex-col items-start justify-start lg:w-3/12 flex py-4 mb-16 lg:mb-0 space-y-5">
-      <a className="btn-accent" href="#1">
+      <button
+        type="button"
+        className="btn-accent"
+        onClick={() => setShowShare(!showShare)}
+      >
         share <i className="fa fa-share" />
-      </a>
+      </button>
+      {showShare && (
+        <div className="space-x-4">
+          <LinkedinShareButton
+            url={window.location.href}
+            title={session.title}
+            summary={truncateString(session.description)}
+          >
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
+
+          <TelegramShareButton url={window.location.href} title={session.title}>
+            <TelegramIcon size={32} round />
+          </TelegramShareButton>
+          <TwitterShareButton
+            url={window.location.href}
+            title={session.title}
+            hashtags={['droidcon', 'droidconKe22', 'dcke22']}
+            related={['droidconke']}
+          >
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+          <WhatsappShareButton url={window.location.href} title={session.title}>
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
+        </div>
+      )}
       <button
         type="button"
         className="btn-primary"
---
+++ utils/helpers.ts
@@ -0,0 +1,33 @@
+import moment from 'moment'
+
+export const isServer = typeof window === 'undefined'
+
+export const isClient = typeof window !== 'undefined'
+
+export const groupBy3 = (arr: string[]) =>
+  arr.reduce(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (acc: any[], current: string, index: number) =>
+      (index % 3 ? acc[acc.length - 1].push(current) : acc.push([current])) &&
+      acc,
+    []
+  )
+
+export const humanReadable = (value: string) =>
+  moment(value).format('MMM Do YY')
+
+export const timeAm = (value: string) => moment(value).format('a')
+
+export const timeDay = (value: string) => moment(value).format('DD')
+
+export const time = (value: string) => moment(value).format('h:mm')
+
+export const hour = (value: string) => moment(value).format('h:mm a')
+
+export const truncateString = (str: string, num = 100) => {
+  if (!str) return ''
+  if (str.length <= num) {
+    return str
+  }
+  return `${str.slice(0, num)}...`
+}
