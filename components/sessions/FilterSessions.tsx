19764982d70c6cd06a5beb43e4c163f5a65749da
Magak Emmanuel <emashmagak@gmail.com>
2022-10-27 10:24:07 +0300

    #56 filter sidebar fixes (#70)

--- components/sessions/FilterSessions.tsx
+++ components/sessions/FilterSessions.tsx
@@ -40,218 +40,225 @@ export const FilterSessions: NextPage<FilterSessionProps> = ({
   }
 
   return (
-    <div className="fixed right-0 top-0 w-25 bg-black z-20 text-white lg:w-1/3 md:w-2/3 h-screen transition-all ease-in-out duration-1000 transform translate-x-0">
-      <div className="bg-black relative">
-        <div className="py-16 px-4 md:px-10">
-          <div className="flex justify-between">
-            <div className="text-secondary text-base">
-              <i className="fa fa-filter text-xl" /> &nbsp; Filter
-            </div>
-            <button
-              type="button"
-              className="text-base hover:cursor-pointer text-light uppercase"
-              onClick={() => setShowFilterSession(false)}
-            >
-              Cancel
-            </button>
-          </div>
-          <div className="mt-10">
-            {groupBy3(sessions3).map((sessions: string[]) => (
-              <div className="flex pb-1" key={sessions[0]}>
-                <button
-                  name="session3"
-                  type="button"
-                  value={sessions[0]}
-                  onClick={onClickFilter}
-                  className={`${
-                    filter?.session3 === sessions[0] && selectedClass
-                  } border rounded-l-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                >
-                  {sessions[0]}
-                </button>
-                <button
-                  name="session3"
-                  type="button"
-                  value={sessions[1]}
-                  onClick={onClickFilter}
-                  className={`${
-                    filter?.session3 === sessions[1] && selectedClass
-                  } border border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                >
-                  {sessions[1]}
-                </button>
-                <button
-                  name="session3"
-                  type="button"
-                  value={sessions[2]}
-                  onClick={(e) => onClickFilter(e)}
-                  className={`${
-                    filter?.session3 === sessions[2] && selectedClass
-                  } border rounded-r-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                >
-                  {sessions[2]}
-                </button>
+    <>
+      <div
+        className="bg-transparent h-full top-0 left-0 z-10 w-1/6 sm:w-3/4 absolute"
+        onClick={() => setShowFilterSession(false)}
+        aria-hidden="true"
+      />
+      <div className="fixed right-0 top-0 w-25 bg-black z-20 text-white lg:w-1/4 md:w-1/3 h-screen transition-all ease-in-out duration-1000 transform translate-x-0">
+        <div className="bg-black relative">
+          <div className="py-16 px-4 md:px-10">
+            <div className="flex justify-between">
+              <div className="text-secondary text-base">
+                <i className="fa fa-filter text-xl" /> &nbsp; Filter
               </div>
-            ))}
-          </div>
-
-          <div className="mt-10">
-            {groupBy3(sessions6).map((sessions: string[]) => (
-              <div className="flex pb-1" key={sessions[0]}>
-                <button
-                  name="session6"
-                  type="button"
-                  value={sessions[0]}
-                  onClick={(e) => onClickFilter(e)}
-                  className={`${
-                    filter?.session6 === sessions[0] && selectedClass
-                  } border rounded-l-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                >
-                  {sessions[0]}
-                </button>
-                {sessions[1] && (
-                  <button
-                    name="session6"
-                    type="button"
-                    value={sessions[1]}
-                    onClick={(e) => onClickFilter(e)}
-                    className={`${
-                      filter?.session6 === sessions[1] && selectedClass
-                    } border border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                  >
-                    {sessions[1]}
-                  </button>
-                )}
-                {sessions[2] && (
-                  <button
-                    name="session6"
-                    type="button"
-                    value={sessions[2]}
-                    onClick={(e) => onClickFilter(e)}
-                    className={`${
-                      filter?.session6 === sessions[2] && selectedClass
-                    } border rounded-r-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                  >
-                    {sessions[2]}
-                  </button>
-                )}
-              </div>
-            ))}
-          </div>
-
-          <div className="mt-10">
-            {groupBy3(sessions4).map((sessions: string[]) => (
-              <div
-                key={sessions[0]}
-                className={`flex pb-1 ${
-                  (sessions.length < 3 &&
-                    sessions.length % 3 === 1 &&
-                    'w-1/3') ||
-                  (sessions.length % 3 === 2 && 'w-2/3')
-                }`}
+              <button
+                type="button"
+                className="text-base hover:cursor-pointer text-light uppercase"
+                onClick={() => setShowFilterSession(false)}
               >
-                <button
-                  name="session4"
-                  type="button"
-                  value={sessions[0]}
-                  onClick={(e) => onClickFilter(e)}
-                  className={`${
-                    filter?.session4 === sessions[0] && selectedClass
-                  } border rounded-l-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                >
-                  {sessions[0]}
-                </button>
-                {sessions[1] && (
-                  <button
-                    name="session4"
-                    type="button"
-                    value={sessions[1]}
-                    onClick={(e) => onClickFilter(e)}
-                    className={`${
-                      filter?.session4 === sessions[1] && selectedClass
-                    } border border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                  >
-                    {sessions[1]}
-                  </button>
-                )}
-                {sessions[2] && (
-                  <button
-                    type="button"
-                    name="session4"
-                    value={sessions[2]}
-                    onClick={(e) => onClickFilter(e)}
-                    className={`${
-                      filter?.session4 === sessions[2] && selectedClass
-                    } border rounded-r-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                  >
-                    {sessions[2]}
-                  </button>
-                )}
-              </div>
-            ))}
-          </div>
-
-          <div className="mt-10">
-            {groupBy3(sessions5).map((sessions: string[]) => (
-              <div
-                key={sessions[0]}
-                className={`flex pb-1 ${
-                  (sessions.length < 3 &&
-                    sessions.length % 3 === 1 &&
-                    'w-1/3') ||
-                  (sessions.length % 3 === 2 && 'w-2/3')
-                }`}
-              >
-                <button
-                  name="session5"
-                  type="button"
-                  value={sessions[0]}
-                  onClick={(e) => onClickFilter(e)}
-                  className={`${
-                    filter?.session5 === sessions[0] && selectedClass
-                  } border rounded-l-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                >
-                  {sessions[0]}
-                </button>
-                {sessions[1] && (
-                  <button
-                    name="session5"
-                    type="button"
-                    value={sessions[1]}
-                    onClick={(e) => onClickFilter(e)}
-                    className={`${
-                      filter?.session5 === sessions[1] && selectedClass
-                    } border border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                  >
-                    {sessions[1]}
-                  </button>
-                )}
-                {sessions[2] && (
-                  <button
-                    type="button"
-                    name="session5"
-                    value={sessions[2]}
-                    onClick={(e) => onClickFilter(e)}
-                    className={`${
-                      filter?.session5 === sessions[2] && selectedClass
-                    } border rounded-r-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
-                  >
-                    {sessions[2]}
-                  </button>
-                )}
-              </div>
-            ))}
-          </div>
-
-          <div className="mt-10">
-            <div className="flex pb-1">
-              <button className="btn-primary w-full uppercase" type="button">
-                filter
+                Cancel
               </button>
+            </div>
+            <div className="mt-10">
+              {groupBy3(sessions3).map((sessions: string[]) => (
+                <div className="flex pb-1" key={sessions[0]}>
+                  <button
+                    name="session3"
+                    type="button"
+                    value={sessions[0]}
+                    onClick={onClickFilter}
+                    className={`${
+                      filter?.session3 === sessions[0] && selectedClass
+                    } border rounded-l-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                  >
+                    {sessions[0]}
+                  </button>
+                  <button
+                    name="session3"
+                    type="button"
+                    value={sessions[1]}
+                    onClick={onClickFilter}
+                    className={`${
+                      filter?.session3 === sessions[1] && selectedClass
+                    } border border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                  >
+                    {sessions[1]}
+                  </button>
+                  <button
+                    name="session3"
+                    type="button"
+                    value={sessions[2]}
+                    onClick={(e) => onClickFilter(e)}
+                    className={`${
+                      filter?.session3 === sessions[2] && selectedClass
+                    } border rounded-r-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                  >
+                    {sessions[2]}
+                  </button>
+                </div>
+              ))}
+            </div>
+
+            <div className="mt-10">
+              {groupBy3(sessions6).map((sessions: string[]) => (
+                <div className="flex pb-1" key={sessions[0]}>
+                  <button
+                    name="session6"
+                    type="button"
+                    value={sessions[0]}
+                    onClick={(e) => onClickFilter(e)}
+                    className={`${
+                      filter?.session6 === sessions[0] && selectedClass
+                    } border rounded-l-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                  >
+                    {sessions[0]}
+                  </button>
+                  {sessions[1] && (
+                    <button
+                      name="session6"
+                      type="button"
+                      value={sessions[1]}
+                      onClick={(e) => onClickFilter(e)}
+                      className={`${
+                        filter?.session6 === sessions[1] && selectedClass
+                      } border border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                    >
+                      {sessions[1]}
+                    </button>
+                  )}
+                  {sessions[2] && (
+                    <button
+                      name="session6"
+                      type="button"
+                      value={sessions[2]}
+                      onClick={(e) => onClickFilter(e)}
+                      className={`${
+                        filter?.session6 === sessions[2] && selectedClass
+                      } border rounded-r-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                    >
+                      {sessions[2]}
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <div className="mt-10">
+              {groupBy3(sessions4).map((sessions: string[]) => (
+                <div
+                  key={sessions[0]}
+                  className={`flex pb-1 ${
+                    (sessions.length < 3 &&
+                      sessions.length % 3 === 1 &&
+                      'w-1/3') ||
+                    (sessions.length % 3 === 2 && 'w-2/3')
+                  }`}
+                >
+                  <button
+                    name="session4"
+                    type="button"
+                    value={sessions[0]}
+                    onClick={(e) => onClickFilter(e)}
+                    className={`${
+                      filter?.session4 === sessions[0] && selectedClass
+                    } border rounded-l-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                  >
+                    {sessions[0]}
+                  </button>
+                  {sessions[1] && (
+                    <button
+                      name="session4"
+                      type="button"
+                      value={sessions[1]}
+                      onClick={(e) => onClickFilter(e)}
+                      className={`${
+                        filter?.session4 === sessions[1] && selectedClass
+                      } border border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                    >
+                      {sessions[1]}
+                    </button>
+                  )}
+                  {sessions[2] && (
+                    <button
+                      type="button"
+                      name="session4"
+                      value={sessions[2]}
+                      onClick={(e) => onClickFilter(e)}
+                      className={`${
+                        filter?.session4 === sessions[2] && selectedClass
+                      } border rounded-r-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                    >
+                      {sessions[2]}
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <div className="mt-10">
+              {groupBy3(sessions5).map((sessions: string[]) => (
+                <div
+                  key={sessions[0]}
+                  className={`flex pb-1 ${
+                    (sessions.length < 3 &&
+                      sessions.length % 3 === 1 &&
+                      'w-1/3') ||
+                    (sessions.length % 3 === 2 && 'w-2/3')
+                  }`}
+                >
+                  <button
+                    name="session5"
+                    type="button"
+                    value={sessions[0]}
+                    onClick={(e) => onClickFilter(e)}
+                    className={`${
+                      filter?.session5 === sessions[0] && selectedClass
+                    } border rounded-l-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                  >
+                    {sessions[0]}
+                  </button>
+                  {sessions[1] && (
+                    <button
+                      name="session5"
+                      type="button"
+                      value={sessions[1]}
+                      onClick={(e) => onClickFilter(e)}
+                      className={`${
+                        filter?.session5 === sessions[1] && selectedClass
+                      } border border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                    >
+                      {sessions[1]}
+                    </button>
+                  )}
+                  {sessions[2] && (
+                    <button
+                      type="button"
+                      name="session5"
+                      value={sessions[2]}
+                      onClick={(e) => onClickFilter(e)}
+                      className={`${
+                        filter?.session5 === sessions[2] && selectedClass
+                      } border rounded-r-md border-1 border-accent px-3 py-1 text-xs flex-1 text-center hover:cursor-pointer hover:bg-accent hover:text-black`}
+                    >
+                      {sessions[2]}
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <div className="mt-10">
+              <div className="flex pb-1">
+                <button className="btn-primary w-full uppercase" type="button">
+                  filter
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
