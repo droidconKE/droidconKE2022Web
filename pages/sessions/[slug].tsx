df0ab67751bb7d8a504f12f35ac953961489890c
Emmanuel Magak <emashmagak@gmail.com>
2022-11-14 14:07:28 +0300

    added twitter header

--- pages/sessions/[slug].tsx
+++ pages/sessions/[slug].tsx
@@ -1,5 +1,6 @@
 import { getCookie } from 'cookies-next'
 import { NextPage } from 'next'
+import Head from 'next/head'
 import Link from 'next/link'
 import { SessionDetails } from '../../components/sessions/SessionDetails'
 import { ShareSessionAndFeedback } from '../../components/sessions/ShareSessionAndFeedback'
@@ -13,33 +14,44 @@ interface SessionPageProp {
 
 const Session: NextPage<SessionPageProp> = ({ session }) => {
   return (
-    <div className="w-full mt-10 mb-0">
-      <section className="w-full bg-dark dark:bg-black-dark">
-        <div className="s-container mt-8 md:mt-0 py-2 md:py-4">
-          <div className="w-full flex items-center space-x-5 mt-5">
-            <Link href="/sessions">
-              <a className="text-white dark:text-white-dark">
-                <i
-                  className="fa fa-arrow-left mr-3"
-                  style={{ transform: 'scale(2.0,0.8)' }}
-                />{' '}
-                back
-              </a>
-            </Link>
-            <h3 className="lowercase text-2xl md:text-3xl text-white dark:text-white-dark">
-              Session Details
-            </h3>
+    <>
+      <Head>
+        <meta
+          name="twitter:image"
+          content={
+            session.session_image ??
+            'https://droidcon.co.ke/images/droidcon-22-kenya-announcement.jpg'
+          }
+        />
+      </Head>
+      <div className="w-full mt-10 mb-0">
+        <section className="w-full bg-dark dark:bg-black-dark">
+          <div className="s-container mt-8 md:mt-0 py-2 md:py-4">
+            <div className="w-full flex items-center space-x-5 mt-5">
+              <Link href="/sessions">
+                <a className="text-white dark:text-white-dark">
+                  <i
+                    className="fa fa-arrow-left mr-3"
+                    style={{ transform: 'scale(2.0,0.8)' }}
+                  />{' '}
+                  back
+                </a>
+              </Link>
+              <h3 className="lowercase text-2xl md:text-3xl text-white dark:text-white-dark">
+                Session Details
+              </h3>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className="s-container py-2 md:py-4">
-        <div className="w-full flex flex-wrap items-start py-0 md:py-3">
-          <SpeakersDetails session={session} />
-          <SessionDetails session={session} />
-          <ShareSessionAndFeedback session={session} />
-        </div>
-      </section>
-    </div>
+        </section>
+        <section className="s-container py-2 md:py-4">
+          <div className="w-full flex flex-wrap items-start py-0 md:py-3">
+            <SpeakersDetails session={session} />
+            <SessionDetails session={session} />
+            <ShareSessionAndFeedback session={session} />
+          </div>
+        </section>
+      </div>
+    </>
   )
 }
 export async function getServerSideProps({
