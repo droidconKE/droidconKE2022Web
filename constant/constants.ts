6b6df56dede52caf7666bb3f574343585c231288
Magak Emmanuel <emashmagak@gmail.com>
2024-05-06 19:57:46 +0300

    added new tickets site

--- constant/constants.ts
+++ constant/constants.ts
@@ -1,2 +1,3 @@
-export const TICKETS_LINK = 'https://bit.ly/getYourDcKE24Tickets'
+// export const TICKETS_LINK = 'https://bit.ly/getYourDcKE24Tickets'
+export const TICKETS_LINK = 'https://bit.ly/dcKE2024Tickets'
 export const CFS_LINK = 'https://bit.ly/dcke24CFP'
