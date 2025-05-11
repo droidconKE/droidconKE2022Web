d63d97e31d6de15dd25b40f67912184d790e7783
NewtonMutugi <newtonmut197@gmail.com>
2025-05-11 17:51:56 +0300

    Updated gallery assets

--- constant/gallery2022.ts
+++ constant/gallery2022.ts
@@ -13,13 +13,13 @@ export const gallery2022 = [
   },
   {
     imageTitle: 'Memories 59',
-    imageName: '59',
+    imageName: 'TTDCON_KEN_1_54',
     imageWidth: 2400,
     imageHeight: 3600,
   },
   {
     imageTitle: 'Memories 308',
-    imageName: '308',
+    imageName: '11_68',
     imageWidth: 2400,
     imageHeight: 3600,
   },
@@ -37,19 +37,19 @@ export const gallery2022 = [
   },
   {
     imageTitle: 'Memories 685',
-    imageName: '685',
+    imageName: '623',
     imageWidth: 2400,
     imageHeight: 3600,
   },
   {
     imageTitle: 'Memories 366',
-    imageName: '255',
+    imageName: 'TTDCON_KEN_39',
     imageWidth: 2400,
     imageHeight: 3600,
   },
   {
     imageTitle: 'Memories 181',
-    imageName: '181',
+    imageName: '6T9B0521',
     imageWidth: 2400,
     imageHeight: 3600,
   },
@@ -67,7 +67,7 @@ export const gallery2022 = [
   },
   {
     imageTitle: 'Memories 467',
-    imageName: '467',
+    imageName: 'TTT_DOC_KI_58',
     imageWidth: 2400,
     imageHeight: 3600,
   },
