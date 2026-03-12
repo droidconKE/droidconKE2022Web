57c7c97fe5f0cf8967d06e1aeea42982bf645225
Magak Emmanuel <emashmagak@gmail.com>
2026-03-12 21:06:05 +0300

    tivket link

--- constant/constants.ts
+++ constant/constants.ts
@@ -1,4 +1,4 @@
 // export const TICKETS_LINK = 'https://bit.ly/getYourDcKE24Tickets'
-export const TICKETS_LINK = 'https://bit.ly/dcKE2025Tickets'
+export const TICKETS_LINK = 'https://bit.ly/dcKE2026Tickets'
 export const CFS_LINK = 'https://bit.ly/dckeCFP26'
 export const SWAG_LINK = 'https://paystack.shop/dcke-fcke-merch-store'
