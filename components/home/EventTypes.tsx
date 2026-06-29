2fcebb2bd977afd254dc0a3aa1c554c589fdfd70
NewtonMutugi <newtonmut197@gmail.com>
2026-06-29 21:24:36 +0300

    feat: replace event type images with custom SVG icons and update layout grid styling

--- components/home/EventTypes.tsx
+++ components/home/EventTypes.tsx
@@ -1,25 +1,42 @@
-import Image from 'next/image'
+import React from 'react'
 
 const eventCards = [
-  { name: 'Panels', image: '/images/new-design/pannels.png', className: '' },
-  { name: 'Workshops', image: '/images/events/workshop.png', className: '' },
+  {
+    name: 'Panels',
+    id: 'panels',
+    image: '/images/svg/panels.svg',
+    className: 'col-span-1 row-span-1',
+  },
+  {
+    name: 'Workshops',
+    id: 'workshops',
+    image: '/images/svg/workshops.svg',
+    className: 'col-span-1 row-span-1',
+  },
   {
     name: 'Speakers',
-    image: '/images/new-design/speakers.png',
-    className: 'row-span-2',
+    id: 'speakers',
+    image: '/images/svg/speakers.svg',
+    className: 'col-span-1 lg:row-span-2 flex-col justify-center',
   },
-  { name: 'Networking', image: '/images/events/party.png', className: '' },
+  {
+    name: 'Networking',
+    id: 'networking',
+    image: '/images/svg/networking.svg',
+    className: 'col-span-1 row-span-1',
+  },
   {
     name: 'Exhibitions',
-    image: '/images/new-design/exhibition.png',
-    className: '',
+    id: 'exhibitions',
+    image: '/images/svg/exhibitions.svg',
+    className: 'col-span-1 row-span-1',
   },
 ]
 
 export const EventTypes = () => {
   return (
     <section className="s-container my-12 md:my-24">
-      <div className="w-full bg-accent rounded-3xl p-8 relative overflow-hidden shadow-xl min-h-[600px] flex flex-col justify-center">
+      <div className="w-full bg-accent rounded-[32px] p-8 md:p-12 relative overflow-hidden shadow-xl min-h-[600px] flex flex-col justify-center">
         {/* Background Image (KICC) */}
         <div className="absolute bottom-0 right-0 w-full md:w-[70%] lg:w-[60%] xl:w-[50%] h-full z-0 flex items-end justify-end pointer-events-none">
           <img
@@ -29,8 +46,8 @@ export const EventTypes = () => {
           />
         </div>
 
-        {/* Content (Left Side) */}
-        <div className="relative z-10 w-full lg:w-3/5">
+        {/* Content */}
+        <div className="relative z-10 w-full lg:w-2/3">
           <div className="flex items-center text-primary text-sm md:text-base font-medium mb-4">
             <div className="w-6 h-px bg-primary mr-3" />
             Event types
@@ -40,21 +57,18 @@ export const EventTypes = () => {
           </h2>
 
           {/* Cards Grid */}
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+          <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 md:gap-4 max-w-xs md:max-w-md lg:max-w-lg">
             {eventCards.map((card) => (
               <div
                 key={card.name}
-                className={`bg-primary rounded-2xl p-4 md:p-6 flex flex-col items-center justify-center min-h-[160px] md:min-h-[200px] ${card.className} ${card.name !== 'Speakers' ? 'aspect-square' : ''}`}
+                className={`bg-primary rounded-[20px] p-4 md:p-5 flex flex-col items-center justify-center ${card.className} ${card.id !== 'speakers' ? 'aspect-square' : ''}`}
               >
-                <div className="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40 relative mb-4">
-                  <Image
-                    src={card.image}
-                    alt={card.name}
-                    layout="fill"
-                    objectFit="contain"
-                  />
-                </div>
-                <h4 className="text-white text-base md:text-lg lg:text-xl font-semibold text-center mt-auto">
+                <img
+                  src={card.image}
+                  alt={card.name}
+                  className="w-10 h-10 md:w-14 md:h-14 lg:w-20 lg:h-20 object-contain"
+                />
+                <h4 className="text-white text-sm md:text-base lg:text-lg  text-center mt-2 md:mt-4">
                   {card.name}
                 </h4>
               </div>
