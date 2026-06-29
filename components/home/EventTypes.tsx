98988893ac7625f30128eeccf4ab7dd135700ab6
NewtonMutugi <newtonmut197@gmail.com>
2026-06-29 17:32:57 +0300

    feat: redesign event types section with new layout and static card configuration

--- components/home/EventTypes.tsx
+++ components/home/EventTypes.tsx
@@ -1,35 +1,65 @@
 import Image from 'next/image'
-import { eventTypes } from '../../constant/eventTypes'
+
+const eventCards = [
+  { name: 'Panels', image: '/images/new-design/pannels.png', className: '' },
+  { name: 'Workshops', image: '/images/events/workshop.png', className: '' },
+  {
+    name: 'Speakers',
+    image: '/images/new-design/speakers.png',
+    className: 'row-span-2',
+  },
+  { name: 'Networking', image: '/images/events/party.png', className: '' },
+  {
+    name: 'Exhibitions',
+    image: '/images/new-design/exhibition.png',
+    className: '',
+  },
+]
 
 export const EventTypes = () => {
   return (
-    <section className="py-12 md:py-10 bg-lighter dark:bg-black-dark">
-      <div className="s-container">
-        <div className="flex justify-center py-2">
-          <h2 className="title lowercase dark:text-accent-dark">
-            <span>event</span> <span className="font-medium">types</span>
-          </h2>
+    <section className="s-container my-12 md:my-24">
+      <div className="w-full bg-accent rounded-4xl p-8 md:p-12 lg:p-16 relative overflow-hidden shadow-xl min-h-[600px] flex flex-col justify-center">
+        {/* Background Image (KICC) */}
+        <div className="absolute bottom-0 right-0 w-full md:w-[70%] lg:w-[60%] xl:w-[50%] h-full z-0 flex items-end justify-end pointer-events-none">
+          <img
+            src="/images/new-design/kenyatta-types.png"
+            alt="KICC"
+            className="w-full h-auto object-contain object-bottom translate-x-4 md:translate-x-12 translate-y-4 md:translate-y-8"
+          />
         </div>
-        <div className="w-full flex flex-wrap mt-4 md:mt-12 dark:text-white-dark">
-          {eventTypes.map((eventType) => {
-            return (
-              <div className="w-1/2 md:w-1/5 mt-10 md:mt-0" key={eventType.id}>
-                <div
-                  className={`px-12 py-10 md:px-16 md:py-12 h-32 md:h-44 ${eventType.lightBG} ${eventType.darkBG} ${eventType.edgeRadius}`}
-                >
+
+        {/* Content (Left Side) */}
+        <div className="relative z-10 w-full lg:w-3/5">
+          <div className="flex items-center text-primary text-sm md:text-base font-medium mb-4">
+            <div className="w-6 h-px bg-primary mr-3" />
+            Event types
+          </div>
+          <h2 className="text-black text-5xl md:text-7xl font-display mb-10">
+            Event Types
+          </h2>
+
+          {/* Cards Grid */}
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+            {eventCards.map((card) => (
+              <div
+                key={card.name}
+                className={`bg-primary rounded-2xl p-4 md:p-6 flex flex-col items-center justify-center min-h-[160px] md:min-h-[200px] ${card.className} ${card.name !== 'Speakers' ? 'aspect-square' : ''}`}
+              >
+                <div className="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40 relative mb-4">
                   <Image
-                    src={`/images/events/${eventType.imageName}.png`}
-                    alt={eventType.imageName}
-                    width={512}
-                    height={512}
+                    src={card.image}
+                    alt={card.name}
+                    layout="fill"
+                    objectFit="contain"
                   />
                 </div>
-                <h4 className="mt-2 font-bold lowercase text-xl">
-                  {eventType.eventName}
+                <h4 className="text-white text-base md:text-lg lg:text-xl font-semibold text-center mt-auto">
+                  {card.name}
                 </h4>
               </div>
-            )
-          })}
+            ))}
+          </div>
         </div>
       </div>
     </section>
